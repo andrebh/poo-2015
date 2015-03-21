8f2f13430df0cabb6bbc2f455fe7947cb685e40d
andre <andresilvacoutinho@gmail.com>
2015-03-20 22:34:04 -0300

    passo2

--- poo-paint/poo-paint/Retangulo.cs
+++ poo-paint/poo-paint/Retangulo.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace poo_paint
 {
+   
     class Retangulo
     {
+        public int x;
+        public int y;
+        public int l;
+        public int a;
+
+        Retangulo(int direito, int esquerdo, int largura, int altura)
+        {
+            this.x = direito;
+            this.y = esquerdo;
+            this.l = largura;
+            this.a = altura;
+        }
+
+       public string Imprime()
+        {
+            string Retangulo = "x" + x + ",y" + y + "l" + l + "a" + a;
+        }
+
         static void Main(string[] args)
         {
+            Retangulo r1 = new Retangulo();
+
         }
     }
 }
