fef49533e426c2cf4994c9a1e8312c056183e4d4
andrebh <andresilvacoutinho@gmail.com>
2015-04-19 11:44:32 -0300

    Teste atv1

--- poo-paint/poo-paint/Program.cs
+++ poo-paint/poo-paint/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace poo_paint
 {
-    class Program
+    class Retangulo
     {
+        public int x;
+        public int y;
+        public int largura;
+        public int altura;
+
+        public Retangulo(int x, int y, int largura, int altura)
+        {
+            this.x = x;
+            this.y = y;
+            this.largura = largura;
+            this.altura = altura;
+        }
+
+        public Imprime()
+        {
+            return "Retangulo [x:"+this.x+" ,y:"+this.y+" ,l:"+this.largura+" ,a:"+this.altura+"]";
+        }
         static void Main(string[] args)
         {
+            
         }
     }
 }
