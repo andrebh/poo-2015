e02a717a1a1aa0e31f33b6839c3fa2d6c4798ecd
andrebh <andresilvacoutinho@gmail.com>
2015-06-26 19:26:57 -0300

    Atividade 8

---
+++ poo-paint/poo-paint/Linha.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_paint
+{
+    public class Linha : Figura
+    {
+        private int priv_larg;
+        private int priv_altu;
+        static int contador;
+
+        public Linha(int x, int y, int largura, int altura)
+        {
+            this.x = x;
+            this.y = y;
+            priv_larg = largura;
+            priv_altu = altura;
+
+            contador += 1;
+        }
+
+        public override string Imprime()
+        {
+            return "linha[x:" + x + ",y:" + y + ",l:" + priv_larg + ",a:" + priv_altu + "]";
+        }
+
+        public static void ZeraContador()
+        {
+            contador = 0;
+        }
+
+        public static int LeContador()
+        {
+            return contador;
+        }
+
+        public override void Desenha(Graphics g)
+        {
+            g.DrawLine(Pens.Black, x, y, priv_larg, priv_altu);
+        }
+    }
+}
